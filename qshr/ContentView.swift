8afad1849beb652dbe9284c76de28ade2a1ffa0f
Shah Qays <klipnov@gmail.com>
2023-08-09 21:45:29 +0800

    Initial Commit

---
+++ qshr/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  qshr
+//
+//  Created by Shah Qays on 09/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ qshr/qshrApp.swift
@@ -0,0 +1,17 @@
+//
+//  qshrApp.swift
+//  qshr
+//
+//  Created by Shah Qays on 09/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct qshrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
