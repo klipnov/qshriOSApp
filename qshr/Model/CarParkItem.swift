a056b6b906f0bc62459419abb8e47651fe8ca603
klipnov <klipnov@gmail.com>
2023-08-10 21:24:07 +0800

    data is categorised

---
+++ qshr/Model/CarParkItem.swift
@@ -0,0 +1,14 @@
+//
+//  CarParkListing.swift
+//  qshr
+//
+//  Created by Shah Qays on 10/08/2023.
+//
+
+import Foundation
+
+struct ListCarParkItem {
+    let carParkNumber: String
+    let totalLots: Int
+    let totalAvailableLots: Int
+}
--- qshr/ViewModel/CarParkViewModel.swift
+++ qshr/ViewModel/CarParkViewModel.swift
@@ -16,7 +16,7 @@ class CarParkViewModel: ObservableObject {
         do {
             if let data = try await CarParksAPI.getCarParks() {
                 await MainActor.run {
-                    processCarParksData(data: data)
+                    processCarParksResponse(response: data)
                 }
             }
         } catch {
@@ -26,12 +26,71 @@ class CarParkViewModel: ObservableObject {
         
     }
     
-    func processCarParksData(data: CarParksResponse?) -> Void {
-        if let carParkItems = data?.items {
-            let count = carParkItems[0].carparkData.count
-            print("There are \(count) car park data items")
+    func processCarParksResponse(response: CarParksResponse?) -> Void {
+        if let carParkItems = response?.items {
+            processCarParkItems(items: carParkItems)
         }
-        carParks = data
+        carParks = response
     }
+    
+    func processCarParkItems(items: [Item]) -> Void {
+        for item in items {
+            processCarParkData(carParkData: item.carparkData)
+        }
+    }
+    
+    func processCarParkData(carParkData: [CarParkData]) -> Void {
+        var listCarParkItems: [ListCarParkItem] = []
+        for item in carParkData {
+            let totalLots = getSumOfTotalLots(carParkInfo: item.carparkInfo)
+            let totalAvailableLots = getSumOfTotalAvailableLots(carParkInfo: item.carparkInfo)
+            
+            let listCarParkItem = ListCarParkItem (
+                carParkNumber: item.carparkNumber,
+                totalLots: totalLots,
+                totalAvailableLots: totalAvailableLots
+            )
+            
+            listCarParkItems.append(listCarParkItem)
+        }
+        
+        categorizeCarParksBasedOnTotalLots(listCarParkItems: listCarParkItems)
+    }
+    
+    func categorizeCarParksBasedOnTotalLots(listCarParkItems: [ListCarParkItem]) -> Void {
+        
+        var small: [ListCarParkItem] = []
+        var medium: [ListCarParkItem] = []
+        var big: [ListCarParkItem] = []
+        var large: [ListCarParkItem] = []
+        
+        for item in listCarParkItems {
+            if (item.totalAvailableLots < 100) {
+                small.append(item)
+            } else if(item.totalAvailableLots >= 100 && item.totalAvailableLots < 300) {
+                medium.append(item)
+            } else if(item.totalAvailableLots >= 300 && item.totalAvailableLots < 400) {
+                big.append(item)
+            } else {
+                large.append(item)
+            }
+        }
+        
+        print(small)
+    }
+    
+    func getSumOfTotalLots(carParkInfo: [CarParkInfo]) -> Int {
+        return carParkInfo.reduce(0) { (partialResult, item) -> Int in
+            return partialResult + (Int(item.totalLots) ?? 0)
+        }
+    }
+    
+    func getSumOfTotalAvailableLots(carParkInfo: [CarParkInfo]) -> Int {
+        return carParkInfo.reduce(0) { (partialResult, item) in
+            return partialResult + (Int(item.lotsAvailable) ?? 0)
+        }
+    }
+    
+    
  
 }
